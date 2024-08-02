c952d3ba9dc704d9c1e66419d2cfa8c9ef0ea17e
Jan Milík <jan.milik@gmail.com>
2024-08-02 19:37:09 +0200

    Make TryClearingCache inlined.

--- cpp_benchmark_common/cpp_benchmark_common.cpp
+++ cpp_benchmark_common/cpp_benchmark_common.cpp
@@ -1,20 +1,2 @@
 #include "cpp_benchmark_common.h"
 
-
-void TryClearingCache(benchmark::State &state)
-{
-    state.PauseTiming();
-
-    constexpr size_t biggerThanCacheSize = 10 * 1024 * 1024;
-    long *bigArray = new long[biggerThanCacheSize];
-
-    memset(bigArray, 0, biggerThanCacheSize * sizeof(long));
-    // for (size_t i = 0; i < biggerThanCacheSize; ++i)
-    //{
-    //    bigArray[i] = 0;
-    // }
-
-    delete[] bigArray;
-
-    state.ResumeTiming();
-}
--- cpp_benchmark_common/cpp_benchmark_common.h
+++ cpp_benchmark_common/cpp_benchmark_common.h
@@ -1,4 +1,30 @@
 #include <benchmark/benchmark.h>
 
+#include <cstdint>
 
-void TryClearingCache(benchmark::State& state);
+
+inline void TryClearingCache(benchmark::State& state)
+{
+   state.PauseTiming();
+
+   const auto& cpuInfo = benchmark::CPUInfo::Get();
+
+   size_t cacheSize = 0;
+   for (const auto& cacheInfo : cpuInfo.caches)
+   {
+      if (cacheInfo.size > cacheSize)
+      {
+         cacheSize = cacheInfo.size;
+      }
+   }
+
+   const size_t biggerThanCacheSize = cacheSize * 2;
+   auto* bigArray = new uint8_t[biggerThanCacheSize];
+
+   memset(bigArray, 0, biggerThanCacheSize * sizeof(uint8_t));
+
+   delete[] bigArray;
+
+   state.ResumeTiming();
+}
+
